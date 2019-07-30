e6244966a8800f6d0ed7f5a23ba3b58b0c9eb1ff
Ali Can Karapazar <ackarapazar@gmail.com>
2019-07-30 14:51:46 +0300

    Add bash script that runs the tap

---
+++ run_tap.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+/home/nolte/.virtualenvs/tap-tempo/bin/python -m tap-tempo.tap_tempo.__init__ -c tap-tempo/tap_config.json -s tap-tempo/state.json | /home/nolte/.virtualenvs/target-stitch/bin/python -m target-stitch.target_stitch.__init__ -c tap-tempo/target_config.json  >> tap-tempo/state.json
+
+tail -1 tap-tempo/state.json > tap-tempo/state.json.tmp && mv tap-tempo/state.json.tmp tap-tempo/state.json
