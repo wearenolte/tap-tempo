f22eba776b7dcabd8e31a71d0e25d18b7647f482
Ali Can Karapazar <ackarapazar@gmail.com>
2019-08-01 11:55:04 +0300

    Test script

--- test_tap.sh
+++ test_tap.sh
@@ -2,4 +2,4 @@
 
 /home/nolte/.virtualenvs/tap-tempo/bin/python -m tap-tempo.tap_tempo.__init__ -c tap-tempo/tap_config.json -s tap-tempo/test_state.json | /home/nolte/.virtualenvs/target-stitch/bin/python -m target-stitch.target_stitch.__init__ -n  >> tap-tempo/state.json
 
-tail -1 tap-tempo/state.json > tap-tempo/state.json.tmp && mv tap-tempo/state.json.tmp tap-tempo/state.json
+tail -1 tap-tempo/test_state.json > tap-tempo/test_state.json.tmp && mv tap-tempo/test_state.json.tmp tap-tempo/test_state.json
